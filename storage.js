97d393207c0a5338bb25be2fbf85c7c6c19fa5bd
Rhys <rhyswoody@gmail.com>
2023-06-27 14:05:58 -0700

    test

--- storage.js
+++ storage.js
@@ -108,13 +108,13 @@ function initStatus(){
 		document.getElementById("wf-form-warehouseForm").reset();}
 	catch(err){} 
 	var disableArray=[
-                    // "whPalletFormat",
-					// "whEndCount",
+                    "whPalletFormat",
+					"whEndCount",
 					// "whPakTechCount",
 					// "whTrayCount",
-					// "whPalletCount",
-					// "whLayerCount",
-					// "whEndCalc",
+					"whPalletCount",
+					"whLayerCount",
+					"whEndCalc"
 					// "labelPSLLength"
                 ];
 
