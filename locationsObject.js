1d8528c2e7929d77e515a6b6c305f6d2f399832e
Rhys Wood <rhys.wood@tricorbraun.com>
2023-09-14 11:44:47 -0700

    update and merge  object to current used object

--- locationsObject.js
+++ locationsObject.js
@@ -12,8 +12,8 @@ const locations = {
 						"Blank Cans" : {
 							min : 10,
 							max : 420,
-							palletOptions : [["Half/Demie",10],["Tall/Grande",17],["Full/Complète",21]]
-						},
+							palletOptions : [["Half/Demie",11],["Tall/Grande",17],["Full/Complète",21]]
+						},	
 						"Shrink Sleeve" : {
 							min : 3,
 							max : 840,
@@ -44,7 +44,7 @@ const locations = {
 							min : 8,
 							max : 640,
 							palletOptions : [["Half/Demie",8],["Short/Courte",13],["Full/Complète",16]]
-						},						
+						},	
 						"Shrink Sleeve" : {
 							min : 3,
 							max : 520,
@@ -75,7 +75,7 @@ const locations = {
 							min : 10,
 							max : 420,
 							palletOptions : [["Half/Demie",8],["Full/Complète",16]]
-						},
+						},	
 						"Shrink Sleeve" : {
 							min : 3,
 							max : 840,
@@ -106,7 +106,7 @@ const locations = {
 				types : ["Standard", "Sleek"]},
 			end : {
 				max : 624,
-				types : ["202 LOE REC Epoxy", "202 LOE 10 STATE USA BPANI", "202 SuperEnd REC Epoxy", "202 SuperEnd 10 STATE USA BPANI"]
+				types : ["202 LOE REC Epoxy", "202 SuperEnd REC Epoxy"]
 			},
 			paktechTypes: ["4pk Black (788/box)","6pk Black (510/box)"]
 		},
@@ -193,7 +193,7 @@ const locations = {
 							min : 3,
 							max : 90,
 							palletOptions : [["Half/Demie",5],["Full/Complète",9]]
-						},
+						}
 					}
 				},
 				"355ml Sleek" : {
@@ -245,7 +245,7 @@ const locations = {
 							min : 10,
 							max : 340,
 							palletOptions : [["Half/Demie",8],["Full/Complète",17]]
-						},	
+						},		
 						"Shrink Sleeve" : {
 							min : 3,
 							max : 680,
@@ -256,7 +256,7 @@ const locations = {
 							max : 680,
 							palletOptions : [["Half/Demie",10],["Full/Complète",17]]
 						},
- 						"PSL": {
+						"PSL" : {
 							min : 3,
 							max : 170,
 							palletOptions : [["Half/Demie",5],["Full/Complète",17]]
@@ -308,7 +308,7 @@ const locations = {
 							min : 11,
 							max : 420,
 							palletOptions : [["Half/Demie",11],["Tall/Grande",17],["Full/Complète",21]]
-						},						
+						},
 						"Shrink Sleeve" : {
 							min : 3,
 							max : 640,
@@ -416,27 +416,27 @@ const locations = {
 							min : 10,
 							max : 340,
 							palletOptions : [["Half/Demie",8],["Full/Complète",17]]
-						},	
+						},
 						"Shrink Sleeve" : {
 							min : 3,
 							max : 680,
 							palletOptions : [["Half/Demie",10],["Full/Complète",17]]
 						},
-                         "SS Label" : {
-							min : 3,
-							max : 680,
-							palletOptions : [["Half/Demie",10],["Full/Complète",17]]
-						},
+                        "SS Label" : {
+                           min : 3,
+                           max : 680,
+                           palletOptions : [["Half/Demie",10],["Full/Complète",17]]
+                       },
 						"PSL" : {
 							min : 3,
 							max : 100,
 							palletOptions : [["Half/Demie",5],["Full/Complète",10]]
 						},
-                         "PSL Label" : {
-							min : 3,
-							max : 100,
-							palletOptions : [["Half/Demie",5],["Full/Complète",10]]
-						}
+                        "PSL Label" : {
+                           min : 3,
+                           max : 100,
+                           palletOptions : [["Half/Demie",5],["Full/Complète",10]]
+                       }
 					}
 				}
 			},
@@ -448,14 +448,14 @@ const locations = {
 			},
 			end: {
 				max : 624,
-				types : ["200 B64 (250 ml Slim Cans Only)", "202 LOE REC Epoxy", "202 SuperEnd REC Epoxy", "202 LOE QC5 BPANI", "202 LOE QC20 BPANI"]
+				types : ["200 B64 (250 ml Slim Cans Only)", "202 SuperEnd REC Epoxy", "202 LOE REC Epoxy", "202 LOE QC5 Epoxy", "202 LOE QC20 Epoxy", "202 LOE 10ST BPANI"]
 			},
 			paktechTypes: ["4pk Black (788/box)", "6pk Black (510/box)", "4pk White (788/box)", "6pk White (510/box)"]
 		},
 		mobileCanning:{
 			labelTypes : ["Silver Bullet / Vierge", "PSL/EAC", "Shrink Sleeve / Manchon rétractable", "Printed/Imprimée", "Cust. Provided/Fourni par le client"],
 			canFormats : ["355ml STD", "473ml STD", "355ml Sleek", "250ml Slim"],
-			trayTypes : ["Standard", "Sleek", "Slim (250ml)", "Waxed Std. (MTL only)"],
+			trayTypes : ["Standard", "Sleek", "Slim (250ml)"],
 			paktechTypes: ["4pk Black (788/box)", "6pk Black (510/box)", "4pk White (788/box)", "6pk White (510/box)"]
 		}
 	}
